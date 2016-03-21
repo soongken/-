983a6da972299187b0f0f051150ecd59cc750944
soongken <lan@lanoudeMac-mini-71.local>
2016-03-21 22:00:58 +0800

    no message

---
+++ 毕业设计/毕业设计/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  毕业设计
+//
+//  Created by lanou on 16/3/21.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 毕业设计/毕业设计/TabViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabViewController.h
+//  毕业设计
+//
+//  Created by lanou on 16/3/21.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabViewController : UITabBarController
+
+@end
